c0eac9e7aeb20d8a2a1c1c9cb43b1818249da727
Dmitriy <natala@MacBook-Pro--Dmitriy.local>
2024-03-07 09:39:18 +0500

    hm4

---
+++ src/main/java/hm4/TeacherView.java
@@ -0,0 +1,11 @@
+package hm4;
+
+import java.util.List;
+
+public class TeacherView {
+    public void printTeachers(List<Teacher> teachers) {
+        for (Teacher teacher : teachers) {
+            System.out.println(teacher);
+        }
+    }
+}
